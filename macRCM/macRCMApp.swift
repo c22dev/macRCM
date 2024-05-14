807f05e1e266fab9c1b144abb121e3923be1a9aa
c22dev <constclerc@gmail.com>
2024-05-14 17:24:06 +0200

    Initial Commit

---
+++ macRCM/macRCMApp.swift
@@ -0,0 +1,17 @@
+//
+//  macRCMApp.swift
+//  macRCM
+//
+//  Created by Constantin Clerc on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct macRCMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
